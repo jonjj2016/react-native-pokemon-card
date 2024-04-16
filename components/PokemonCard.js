282095b0dd26b4ac926bb13e60265cfc05c17206
Jon <jonmartirosyan1988@gmail.com>
2024-04-16 15:19:32 +0700

    added card and assets for pokemon cards

---
+++ components/PokemonCard.js
@@ -0,0 +1,34 @@
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View, Platform } from 'react-native'
+
+export default function PokemonCard() {
+  return (
+    <View style={styles.card}>
+      <Text>Pokemon Card</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: 'white',
+    borderRadius: 16,
+    borderWidth: 2,
+    padding: 16,
+    margin: 16,
+    ...Platform.select({
+      ios: {
+        shadowffset: {
+          width: 2,
+          height: 2,
+        },
+        shadowColor: '#333',
+        shadowOpacity: 0.3,
+        shadowRadius: 4,
+      },
+      android: {
+        elevation: 5,
+      },
+    }),
+  },
+})
